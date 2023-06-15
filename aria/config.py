599c9d512c5bf9aeb851fdba0ada5aa16a0fa8fa
Louis Bradshaw <Louisb19@live.co.uk>
2023-06-15 13:21:44 +0100

    change config and tests structure

--- aria/config.py
+++ aria/config.py
@@ -1,12 +1,16 @@
-"""Includes functionality for loading config.json"""
+"""Includes functionality for loading config files."""
 
+import os
 import json
 
-CONFIG_PATH = "./aria/config.json"
+CONFIG_DIR = os.path.join(os.path.dirname(__file__), "../config/")
 
 
 def load_config():
-    with open(CONFIG_PATH) as f:
-        config = json.load(f)
+    with open(os.path.join(CONFIG_DIR, "config.json")) as f:
+        return json.load(f)
 
-    return config
+
+def load_model_config(name: str):
+    with open(os.path.join(CONFIG_DIR, f"{name}.json")) as f:
+        return json.load(f)
